f69a91df8c4107dbe08f042ea5e28f84dd63f3eb
Anthony Leif <tonyleif@hotmail.com>
2017-09-22 21:14:48 -0500

    Page 208

---
+++ SportsStore.WebUI/Controllers/NavController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+using SportsStore.Domain.Abstract;
+
+namespace SportsStore.WebUI.Controllers
+{
+    public class NavController : Controller
+    {
+        //private IProductRepository repository;
+
+        //public NavController(IProductRepository repo)
+        //{
+        //    repository = repo;
+        //}
+        public string Menu()
+        {
+            return "Hello from NavController";
+        }
+    }
+}
