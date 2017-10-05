309eef90c9656ab89bec38ffb6c1850ce282b0aa
Anthony Leif <tonyleif@hotmail.com>
2017-10-04 22:53:07 -0500

    Page 275

---
+++ SportsStore.WebUI/Controllers/AdminController.cs
@@ -0,0 +1,19 @@
+﻿using System.Web.Mvc;
+using SportsStore.Domain.Abstract;
+
+namespace SportsStore.WebUI.Controllers
+{
+    public class AdminController : Controller
+    {
+        IProductRepository repository;
+
+        public AdminController(IProductRepository repo)
+        {
+            repository = repo;
+        }
+        public ViewResult Index()
+        {
+            return View(repository.Products);
+        }
+    }
+}
