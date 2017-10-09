322640de77bea2f79a6ab7f668dba831c4c8d77f
Anthony Leif <tonyleif@hotmail.com>
2017-10-09 16:23:16 -0500

    Page 285

--- SportsStore.Domain/Entities/Product.cs
+++ SportsStore.Domain/Entities/Product.cs
@@ -1,14 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.ComponentModel.DataAnnotations;
+using System.Web.Mvc;
 
 namespace SportsStore.Domain.Entities
 {
     public class Product
     {
+        [HiddenInput(DisplayValue = false)]
         public int ProductID { get; set; }
+        [DataType(DataType.MultilineText)]
         public string Name { get; set; }
         public string Description { get; set; }
         public decimal Price { get; set; }
